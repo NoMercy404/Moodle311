bb2e59911215f867a88e29c87e54c43daaea9508
NoMercy404 <majchrowski215@gmail.com>
2026-05-27 19:54:01 +0100

    first commit

---
+++ blocks/testblock/db/access.php
@@ -0,0 +1,21 @@
+<?php
+$capabilities = [
+    'block/testblock:myaddinstance' => [
+        'captype' => 'write',
+        'contextlevel' => CONTEXT_SYSTEM,
+        'archetypes' => [
+            'user' => CAP_ALLOW
+        ],
+        'clonepermissionsfrom' => 'moodle/my:manageblocks'
+    ],
+    'block/testblock:addinstance' => [
+        'riskbitmask' => RISK_SPAM | RISK_XSS,
+        'captype' => 'write',
+        'contextlevel' => CONTEXT_BLOCK,
+        'archetypes' => [
+            'editingteacher' => CAP_ALLOW,
+            'manager' => CAP_ALLOW
+        ],
+        'clonepermissionsfrom' => 'moodle/site:manageblocks'
+    ],
+];
---
+++ blocks/testblock/lang/pl/block_testblock.php
@@ -0,0 +1,36 @@
+<?php
+// This file is part of Moodle - http://moodle.org/
+//
+// Moodle is free software: you can redistribute it and/or modify
+// it under the terms of the GNU General Public License as published by
+// the Free Software Foundation, either version 3 of the License, or
+// (at your option) any later version.
+//
+// Moodle is distributed in the hope that it will be useful,
+// but WITHOUT ANY WARRANTY; without even the implied warranty of
+// MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+// GNU General Public License for more details.
+//
+// You should have received a copy of the GNU General Public License
+// along with Moodle.  If not, see <http://www.gnu.org/licenses/>.
+
+/**
+ * Strings for component 'block_html', language 'en', branch 'MOODLE_20_STABLE'
+ *
+ * @package   block_html
+ * @copyright 1999 onwards Martin Dougiamas  {@link http://moodle.com}
+ * @license   http://www.gnu.org/copyleft/gpl.html GNU GPL v3 or later
+ */
+
+$string['allowadditionalcssclasses'] = 'Zezwol na dodatkowy CSS';
+$string['configallowadditionalcssclasses'] = 'Adds a configuration option to HTML block instances allowing additional CSS classes to be set.';
+$string['configclasses'] = 'Dodatkowe klasy css';
+$string['configclasses_help'] = 'jakies pierdolenie kota w ogon';
+$string['configcontent'] = 'Zawartosc';
+$string['configtitle'] = 'tytul bloku testowego ';
+$string['html:addinstance'] = 'dodaj nowy blok';
+$string['html:myaddinstance'] = 'dodaj nowy blok do dashboardu';
+$string['newhtmlblock'] = '(nowy testowy blok)';
+$string['pluginname'] = 'TEST BLOK';
+$string['search:content'] = 'zawartosc bloku HTML';
+$string['privacy:metadata:block'] = 'ten blok przechowuje wszystkie potrzebne dla siebie dane w obrebie tego bloku.';
